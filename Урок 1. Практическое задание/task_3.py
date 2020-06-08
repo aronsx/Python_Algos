f0e3d4be94566e5e181ab5298ac23752dca92109
aronsx <aronsx@ya.ru>
2020-06-08 13:12:57 +0300

    task3 is done

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -13,3 +13,15 @@ k – угловой коэффициент (действительное чис
 X1_VAL = 2, Y1_VAL = 3, X2_VAL = 4, Y2_VAL = 5
 Уравнение прямой, проходящей через эти точки: y = 1.0x + 1.0
 """
+
+X1_VAL = int(input("Введите x1: "))
+Y1_VAL = int(input("Введите y1: "))
+X2_VAL = int(input("Введите x2: "))
+Y2_VAL = int(input("Введите y2: "))
+
+try:
+    k = (Y2_VAL - Y1_VAL) / (X2_VAL - X1_VAL)
+    b = Y1_VAL - k * X1_VAL
+    print(f'Уравнение прямой: y = {k}x + {b}')
+except ZeroDivisionError as zero_err:
+    print('Ошибка:', zero_err)
