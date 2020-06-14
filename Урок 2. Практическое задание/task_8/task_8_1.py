3dfa481a744693d8c19ddf847e0b73a2b1c83846
aronsx <aronsx@ya.ru>
2020-06-14 03:05:36 +0300

    task8.1 is done (while)

--- Урок 2. Практическое задание/task_8/task_8_1.py
+++ Урок 2. Практическое задание/task_8/task_8_1.py
@@ -12,3 +12,18 @@
 
 ЗДЕСЬ ДОЛЖНА БЫТЬ РЕАЛИЗАЦИЯ ЧЕРЕЗ ЦИКЛ
 """
+from random import randint
+
+user_count = int(input('Сколько будет чисел? - '))
+search_num = int(input('Какую цифру считать? - '))
+COUNT_NUM = 0
+
+for idx in range(1, user_count + 1):
+    current_num = randint(1, 1000)
+    print(f'Число {idx}: {current_num}')
+    while current_num:
+        if current_num % 10 == search_num:
+            COUNT_NUM += 1
+        current_num //= 10
+
+print(f"Было введено {COUNT_NUM} цифр '{search_num}'")
