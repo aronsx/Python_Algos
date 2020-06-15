a3d778c99b24b3113402cdab8d212aa4b46301a7
aronsx <aronsx@ya.ru>
2020-06-16 01:09:16 +0300

    task 4 done

--- Урок 3.Практическое задание/task_4.py
+++ Урок 3.Практическое задание/task_4.py
@@ -3,3 +3,11 @@
 
 Подсказка: можно применить ф-цию max с параметром key
 """
+list_a = [88, 26, 41, 75, 88, 23, 52, 88, -49, 60, 69, -18]
+
+MAX_NUM = 0
+for num, val in enumerate(list_a):
+    if list_a.count(val) > MAX_NUM:
+        MAX_NUM = num
+
+print(list_a[MAX_NUM])
