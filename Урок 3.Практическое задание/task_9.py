4d748fd1d89d8614e3bb7f2f1efe87d0341a894a
aronsx <aronsx@ya.ru>
2020-06-17 18:19:05 +0300

    task 9 done

--- Урок 3.Практическое задание/task_9.py
+++ Урок 3.Практическое задание/task_9.py
@@ -11,3 +11,35 @@
 [13, 12, 7, 15] минимальные значения по столбцам
 Максимальное среди них = 15
 """
+from random import randint
+
+rows = int(input('Задайте количество строк в матрице: '))
+columns = int(input('Задайте количество столбцов в матрице: '))
+
+# заполнение матрицы рандомными зничениями
+matrix_a = [[randint(10, 100) for _ in range(columns)] for _ in range(rows)]
+# print(matrix_a)
+
+# печать матрицы
+for row in matrix_a:
+    for elem in row:
+        print(elem, end='\t')
+    print()
+
+# поиск минимальных значений в столбцах
+min_of_column = []
+for column in range(columns):
+    cache = 100
+    for row in range(rows):
+        if matrix_a[row][column] < cache:
+            cache = matrix_a[row][column]
+    min_of_column.append(cache)
+
+print(f'{min_of_column} минимальные значения по столбцам')
+
+# поиск максимального значения
+max_row_num = 0
+for item in min_of_column:
+    if item > max_row_num:
+        max_row_num = item
+print(f'Максимальное среди них = {max_row_num}')
