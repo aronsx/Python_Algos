78697f75ba40c1e9245c7eea43f5731fc47bd452
aronsx <aronsx@ya.ru>
2020-06-26 02:52:11 +0300

    работает правильно вывод i-того элемента в обоих функциях

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -28,21 +28,36 @@ def get_elem_i(func):
         i = kwargs.get('i')
         a = func(*args, **kwargs)
         return a[i - 1]
+
     return wrapper
 
 
-@get_elem_i
+def modify_list(func):
+    def wrapper(*args, **kwargs):
+        i = kwargs.get('i')
+        _list = get_list(i)
+        a = func(_list, **kwargs)
+        return a[i - 1]
+
+    return wrapper
+
+
+# @get_elem_i
+@modify_list
 def eratosfen(input_list, **kwargs):
-    for divider in input_list:
-        for divided in input_list:
+    _list = input_list[:]
+    for divider in _list:
+        for divided in _list:
             if divider != divided and divided % divider == 0:
-                list_a.remove(divided)
-    return input_list
+                _list.remove(divided)
+    return _list
 
 
-@get_elem_i
+# @get_elem_i
+@modify_list
 def linear_simple(input_list, **kwargs):
     """Решение без метода решета Эратосфена, можно передать генератор"""
+
     simple_list = [2]
     for divider in input_list:
         count = 0
@@ -54,14 +69,30 @@ def linear_simple(input_list, **kwargs):
     return simple_list[1:]
 
 
-len_list = 100
-list_a = [item for item in range(2, len_list + 1)]
-list_b = list(range(len_list + 1)[2:])
-list_c = (item for item in range(2, len_list + 1))
-list_d = {item for item in range(2, len_list + 1)}
-list_e = num_generator()
+def get_list(i):  # генерю каждый раз исключительно для тестов
+    _len = i * 10
+    _list = [item for item in range(2, _len + 1)]
+    return _list
 
-print(linear_simple(list_c, i=10))
-print(eratosfen(list_a, i=10))
-# print(linear_simple(list_e))
-# print(eratosfen(list_e))
+
+i_elem = 10
+len_list = i_elem * 10
+list_a = [item for item in range(2, len_list + 1)]
+list_f = list_a[:]
+# list_b = list(range(len_list + 1)[2:])
+# list_c = (item for item in range(2, len_list + 1))
+# list_d = {item for item in range(2, len_list + 1)}
+# list_e = num_generator()
+
+# print(linear_simple(list_f, i=i_elem))
+print(linear_simple(i=1))
+print(linear_simple(i=10))
+print(linear_simple(i=100))
+print(linear_simple(i=1000))
+# print(eratosfen(list_a, i=i_elem))
+print(eratosfen(i=1))
+print(eratosfen(i=10))
+print(eratosfen(i=100))
+print(eratosfen(i=1000))
+
+# print(timeit('eratosfen(list_a, 1)', setup='from __main__ import eratosfen')
