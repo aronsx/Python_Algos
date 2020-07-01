0c7a7e7738f5a199d5edef55a9897e71dccefabb
aronsx <aronsx@ya.ru>
2020-07-01 13:58:14 +0300

    task_1 done.

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -9,3 +9,41 @@
 Подсказка: обратите внимание, сортируем не по возрастанию, как в примере,
 а по убыванию
 """
+from random import randrange
+from timeit import timeit
+
+
+def bubble(lst):
+    _list = lst[:]
+    n = 1
+    while n < len(_list):
+        for idx in range(len(_list) - n):
+            if _list[idx] < _list[idx + 1]:
+                _list[idx], _list[idx + 1] = _list[idx + 1], _list[idx]
+        n += 1
+    return _list
+
+
+def bubble_opt(lst):
+    _list = lst[:]
+    n = 1
+    while n < len(_list):
+        change = False
+        for idx in range(len(_list) - n):
+            if _list[idx] < _list[idx + 1]:
+                _list[idx], _list[idx + 1] = _list[idx + 1], _list[idx]
+                if not change:  # без проверки на каждой итерации в переменную присваивалось True, что в конечном итоге замедляло работу функции и она отрабатывала медленнее, чем первая функция.
+                    change = True
+        if not change:
+            break
+        n += 1
+    return _list
+
+
+list_a = [randrange(-100, 100) for _ in range(1000)]
+print(list_a)  # исходный список
+print(bubble(list_a))  # результат работы первой функции
+print(bubble_opt(list_a))  # результат работы второй функции
+
+print(timeit('bubble(list_a)', setup='from __main__ import bubble, list_a', number=100))
+print(timeit('bubble_opt(list_a)', setup='from __main__ import bubble_opt, list_a', number=100))
