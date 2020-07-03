0f92a4ebef0cf2f87d53eed23f3076e4ff379a99
aronsx <aronsx@ya.ru>
2020-07-04 01:26:13 +0300

    task_1 done

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,22 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+a = 'papa'
+
+set_ = set()
+
+count = 0
+for i in range(len(a)):
+    for j in range(len(a) + 1):
+        if j <= i:
+            continue
+        _str = a[i:j]
+        cache = hashlib.md5(_str.encode()).hexdigest()
+        if cache not in set_ and _str != a:
+            print(_str)
+            count += 1
+        set_.add(cache)
+
+print(f'Итог: {count} подстрок')
